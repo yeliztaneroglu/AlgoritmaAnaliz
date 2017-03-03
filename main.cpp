e143e20400683cc0ea62917fce37d6208d83888c
Yeliz Taneroğlu <yeliztaneroglu@gmail.com>
2017-03-03 14:13:08 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,22 +1,43 @@
+#include <cstdlib>
 #include <iostream>
 
 
 using namespace std;
-
-
-int power(int a, int b){
-    int toplam = 1;
+int step=0;
+int step2=0;
+long long power(long long x, long long y) {
+     step++;
+    if(y == 0) 
+    {  
+         return 1;
+    }
+    long long d = power(x, y/2);
+    if(y%2 == 0) 
+    {          
+           return d*d;
+    }
+    else 
+    {      
+         return x*d*d;
+    }
+}
+long long power2(long long a, long long b){
+    long long toplam = 1;
+    step++;
     for(int i=0; i<b; i++){
+            step2++;
         toplam *= a;
     }
     return toplam;
 }
-
-int main()
+int main(int argc, char *argv[])
 {
-    int sonuc = power(2,4);
-    cout << sonuc << endl;
-    return 0;
+    cout<<power(2,60)<<endl;
+    cout<<step<<endl;
+    cout<<power2(2,60)<<endl;
+    cout<<step2<<endl;
+    system("PAUSE");
+    return EXIT_SUCCESS;
 }
 
 
